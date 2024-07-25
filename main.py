bc9ada6b4ffb63a4b64e4596a892698962963451
Doodhust <dimaggg2837@gmail.com>
2024-07-25 15:04:25 +1000

    analyzing

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ import pandas as pd
 
 
 def get_data(name_korpus):
-    conn = connect(host='localhost', database='zxc', user='postgres', password='12345')
+    # your seettings
+    conn = connect(host='localhost', database='admin', user='admin', password='admin')
     cur = conn.cursor()
 
     if name_korpus == 'D':
-        query = "SELECT * FROM microclim_records WHERE device_id <= 20 OR device_id IN (78, 255, 257, 258)"
+        query = "SELECT * FROM public.microclim_records WHERE device_id <= 20 OR device_id IN (78, 255, 257, 258)"
     elif name_korpus == 'A':
-        query = "SELECT * FROM microclim_records WHERE device_id >= 33 AND device_id <= 69 OR device_id = 28"
+        query = "SELECT * FROM public.microclim_records WHERE device_id >= 33 AND device_id <= 69 OR device_id = 28"
     elif name_korpus == 'S':
-        query = "SELECT * FROM microclim_records WHERE device_id >= 220 AND device_id <= 253 OR device_id IN (29, 30)"
+        query = "SELECT * FROM public.microclim_records WHERE device_id >= 220 AND device_id <= 253 OR device_id IN (29, 30)"
     elif name_korpus == 'E':
-        query = "SELECT * FROM microclim_records WHERE device_id >= 21 AND device_id <= 27 OR device_id IN (79, 80)"
+        query = "SELECT * FROM public.microclim_records WHERE device_id >= 21 AND device_id <= 27 OR device_id IN (79, 80)"
     elif name_korpus == 'B':
-        query = "SELECT * FROM microclim_records WHERE device_id >= 70 AND device_id <= 77"
+        query = "SELECT * FROM public.microclim_records WHERE device_id >= 70 AND device_id <= 77"
     elif name_korpus == 'M':
-        query = "SELECT * FROM microclim_records WHERE device_id >= 188 AND device_id <= 219"
+        query = "SELECT * FROM public.microclim_records WHERE device_id >= 188 AND device_id <= 219"
     elif name_korpus == 'L':
-        query = "SELECT * FROM microclim_records WHERE device_id >= 91 AND device_id <= 193"
+        query = "SELECT * FROM public.microclim_records WHERE device_id >= 91 AND device_id <= 193"
 
     cur.execute(query)
     results = cur.fetchall()
@@ -53,37 +54,33 @@ def df_reset_index(list_df):
     return list_df
 
 # Корпус D (700k)
-#data = get_data('D')
-#df_D = to_df(data)
+data = get_data('D')
+df_D = to_df(data)
 
 # Корпус E (152k)
 data = get_data('E')
 df_E = to_df(data)
 
+
 # Корпус A (1168k)
-#data = get_data('A')
-#df_A = to_df(data)
+data = get_data('A')
+df_A = to_df(data)
 
 # Корпус S (845k)
-#data = get_data('S')
-#df_S = to_df(data)
+data = get_data('S')
+df_S = to_df(data)
 
 # Корпус B (244k)
-#data = get_data('B')
-#df_B = to_df(data)
+data = get_data('B')
+df_B = to_df(data)
 
 # Корпус L (833k)
-#data = get_data('L')
-#df_L = to_df(data)
+data = get_data('L')
+df_L = to_df(data)
 
 # Корпус M (799k)
-#data = get_data('M')
-#df_M = to_df(data)
+data = get_data('M')
+df_M = to_df(data)
 
 
-
-df_E = date_format(df_E)
-df_E = df_reset_index(df_E)
-
-
-print(df_E[2])
+print(df_E.head())
